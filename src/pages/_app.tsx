540919a4124b490cfff8e8995a978e7e62ec3c13
JKapturkiewicz <kuba.kapek@gmail.com>
2023-06-27 17:07:22 +0200

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
-import '@/styles/globals.css'
+import "@/styles/main.scss";
+import "@fontsource/k2d";
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
