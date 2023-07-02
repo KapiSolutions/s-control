8ed23a53feb00c72bba5b8faf0bd9ba89b691c4b
JKapturkiewicz <kuba.kapek@gmail.com>
2023-07-02 21:00:34 +0200

    Add admin welcome page

---
+++ src/pages/admin/index.tsx
@@ -0,0 +1,27 @@
+// import BreadCrumbs from "@/components/BreadCrumbs";
+import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { UserProfile } from "@auth0/nextjs-auth0/client";
+import { Typography, Box } from "@mui/material";
+// import { NextSeo } from "next-seo";
+
+type ProfileProps = { user: UserProfile };
+
+export default function AdminPage({ user }: ProfileProps): JSX.Element {
+  // console.log(user);
+  const breadcrumbs = [{ name: "Login Page", path: "/admin" }];
+  return (
+    <>
+      {/* <NextSeo title="S-Control | Successful Login!" nofollow={true} /> */}
+
+      <Box sx={{ mt: 5, ml: 2 }}>
+        {/* <BreadCrumbs items={breadcrumbs} /> */}
+        <Typography variant="body1">
+          {/* Welcome {user.name}! <a href="/api/auth/logout"> - Logout</a> */}
+          Welcome back!
+        </Typography>
+      </Box>
+    </>
+  );
+}
+
+export const getServerSideProps = withPageAuthRequired();
