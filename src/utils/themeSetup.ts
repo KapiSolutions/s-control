1c6b351b0b55452f3fe1a8bf7a39678b38f40118
JKapturkiewicz <kuba.kapek@gmail.com>
2023-07-02 16:15:20 +0200

    Create theme setup

---
+++ src/utils/themeSetup.ts
@@ -0,0 +1,33 @@
+const themeSetup = {
+  palette: {
+    text: {
+      primary: "#001525",
+    },
+    primary: {
+      main: "#f7b236",
+      dark: "#f09e0a",
+      // contrastText: "#f1f1f1",
+    },
+  },
+  typography: {
+    allVariants: {
+      fontFamily: [
+        "Roboto",
+        "-apple-system",
+        "BlinkMacSystemFont",
+        '"Segoe UI"',
+        '"Helvetica Neue"',
+        "Arial",
+        "sans-serif",
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+      ].join(","),
+    },
+    h1: {
+      fontFamily: "teko",
+    },
+  },
+};
+
+export default themeSetup;
