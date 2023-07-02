10e4badb4167c8c102050399b1e648f3cab72d98
JKapturkiewicz <kuba.kapek@gmail.com>
2023-07-02 19:32:08 +0200

    Add Layout wrapper, mui ThemeProvider and auth0 UserProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,23 @@
 import "@/styles/main.scss";
-import "@fontsource/k2d";
-import type { AppProps } from 'next/app'
+import "@fontsource/teko";
+import "@fontsource/roboto";
+import Layout from "@/components/layout/Layout";
+import { UserProvider } from "@auth0/nextjs-auth0/client";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import type { AppProps } from "next/app";
+import themeSetup from "@/utils/themeSetup";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const theme = createTheme(themeSetup);
+  return (
+    <UserProvider>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </UserProvider>
+  );
 }
