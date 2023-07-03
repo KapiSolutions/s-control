4dbdb09a595bfa19438aff0554f33ef70a337507
JKapturkiewicz <kuba.kapek@gmail.com>
2023-07-03 15:49:20 +0200

    Create content header component

---
+++ src/components/ContentHeader.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
+
+// Define types
+type Props = {
+  primary: string;
+  secondary: string;
+};
+
+const ContentHeader = ({ primary, secondary }: Props): JSX.Element => {
+  return (
+    <Stack direction="row" spacing={2} sx={{mb:4}}>
+      <Box sx={{ position: "relative", width: "8px", minHeight: "100%", backgroundColor: "primary.main" }}></Box>
+      <Box>
+        <Typography variant="h4" sx={{ textTransform: "uppercase", fontWeight: "bold" }}>
+          {primary}
+        </Typography>
+        <Typography variant="body1">{secondary}</Typography>
+      </Box>
+    </Stack>
+  );
+};
+
+export default ContentHeader;
