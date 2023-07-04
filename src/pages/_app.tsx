bb6364274af01858e1b108e95c1d431e7759f875
JKapturkiewicz <kuba.kapek@gmail.com>
2023-07-04 19:12:26 +0200

    Add SnackbarProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,12 @@
 import "@/styles/main.scss";
 import "@fontsource/teko";
-import '@fontsource/poppins/400.css';
-import '@fontsource/poppins/600.css';
+import "@fontsource/poppins/400.css";
+import "@fontsource/poppins/600.css";
 import Layout from "@/components/layout/Layout";
 import { UserProvider } from "@auth0/nextjs-auth0/client";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
+import { SnackbarProvider } from "notistack";
 import type { AppProps } from "next/app";
 import themeSetup from "@/utils/themeSetup";
 
@@ -15,9 +16,11 @@ export default function App({ Component, pageProps }: AppProps) {
     <UserProvider>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
+        <SnackbarProvider maxSnack={3}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </SnackbarProvider>
       </ThemeProvider>
     </UserProvider>
   );
