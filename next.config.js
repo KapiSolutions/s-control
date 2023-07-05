84e95b884b8d0c4cda9cf0036589394cde15df0d
JKapturkiewicz <kuba.kapek@gmail.com>
2023-07-05 15:17:04 +0200

    Add headers config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,42 @@
 /** @type {import('next').NextConfig} */
+const production = process.env.NODE_ENV == "production";
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "X-DNS-Prefetch-Control",
+            value: "on",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: `default-src 'self' https://vitals.vercel-insights.com https://www.google.com/recaptcha/ ; form-action 'self'; frame-src 'self' https://www.google.com/recaptcha/; style-src 'self' https://fonts.googleapis.com 'unsafe-inline'; img-src 'self' https://storage.googleapis.com data: ; script-src 'self' ${
+              production ? "" : "'unsafe-eval'"
+            } https://apis.google.com  https://vitals.vercel-insights.com/  https://www.google.com/recaptcha/ https://www.gstatic.com/recaptcha/ ;`,
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+          {
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
+          },
+        ],
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
