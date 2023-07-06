5d5bbf2e9aed5957627e415871ef38ffdf1592ea
JKapturkiewicz <kuba.kapek@gmail.com>
2023-07-06 16:04:32 +0200

    Update data

--- src/utils/schema/realization.ts
+++ src/utils/schema/realization.ts
@@ -2,37 +2,37 @@ import * as yup from "yup";
 
 export const initValues = {
   title: "",
-  mainImage: "",
-  realizationDate: null,
-  publishedDate: new Date(),
   author: "S-control",
-  description: "",
-  atrPower: "",
   atrType: "",
+  atrLocalization: "",
+  atrPower: "",
   atrPanels: "",
   atrInverter: "",
   atrPump: "",
-  atrLocalization: "",
-  images: "",
   tags: "",
+  realizationDate: null,
+  publishedDate: new Date(),
+  mainImage: "",
+  images: "",
+  description: "",
 };
 
 export type Realization = {
   _id?: string;
   title: string;
-  mainImage: string;
-  realizationDate: null | Date;
-  publishedDate: null | Date;
   author: string;
-  description: string;
-  atrPower: string;
   atrType: string;
+  atrLocalization: string;
+  atrPower: string;
   atrPanels: string;
   atrInverter: string;
   atrPump: string;
-  atrLocalization: string;
-  images: string;
   tags: string;
+  realizationDate: null | Date;
+  publishedDate: null | Date;
+  mainImage: string;
+  images: string;
+  description: string;
 };
 export interface Realizations extends Array<Realization> {}
 
@@ -42,17 +42,23 @@ const maxLength = 1000;
 
 export const schema = yup.object().shape({
   title: yup.string().required(reqMsg).max(maxLength, limitMsg),
-  mainImage: yup.string().required(reqMsg).max(maxLength, limitMsg),
-  realizationDate: yup.date().default(() => new Date()),
-  publishedDate: yup.date().default(() => new Date()),
   author: yup.string().required(reqMsg).max(maxLength, limitMsg),
-  description: yup.string().required(reqMsg).max(maxLength, limitMsg),
-  atrPower: yup.string().max(maxLength, limitMsg),
   atrType: yup.string().max(maxLength, limitMsg),
+  atrLocalization: yup.string().max(maxLength, limitMsg),
+  atrPower: yup.string().max(maxLength, limitMsg),
   atrPanels: yup.string().max(maxLength, limitMsg),
   atrInverter: yup.string().max(maxLength, limitMsg),
   atrPump: yup.string().max(maxLength, limitMsg),
-  atrLocalization: yup.string().max(maxLength, limitMsg),
-  images: yup.string().max(maxLength, limitMsg),
   tags: yup.string().max(maxLength, limitMsg),
+  realizationDate: yup
+    .date()
+    .required(reqMsg)
+    .default(() => new Date()),
+  publishedDate: yup
+    .date()
+    .required(reqMsg)
+    .default(() => new Date()),
+  mainImage: yup.string().required(reqMsg).max(maxLength, limitMsg),
+  images: yup.string().max(maxLength, limitMsg),
+  description: yup.string().required(reqMsg).max(maxLength, limitMsg),
 });
