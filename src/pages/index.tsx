8a8270ab5113ffe132a6e8677b2470cb1d7b2ebf
JKapturkiewicz <kuba.kapek@gmail.com>
2023-07-07 19:54:33 +0200

    Get and sort data from mongodb

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,14 +5,18 @@ import WhyUs from "@/components/sections/WhyUs";
 import Dotation from "@/components/sections/Dotation";
 import Contact from "@/components/sections/Contact";
 import Realizations from "@/components/sections/realizations/Realizations";
-import realizations from "@/tmp/realizations.json";
+import { connectDB, client } from "@/utils/mongodb";
 
-export default function Home() {
+type Props = {
+  realizations: Realizations;
+};
+
+export default function HomePage({ realizations }: Props): JSX.Element {
   return (
     <>
       <NextSeo
         title="S-control | Twoja fotowoltaika"
-        // description={t[locale].desc}
+        description="Opis"
         additionalLinkTags={[
           {
             rel: "icon",
@@ -26,9 +30,44 @@ export default function Home() {
       <Stack spacing={6}>
         <WhyUs />
         <Dotation />
-        <Contact />
         <Realizations realizations={realizations} />
+        <Contact />
       </Stack>
     </>
   );
+}
+
+export async function getStaticProps() {
+  const dbName = "Data";
+  const collectionName = "Realizations";
+  let sortedrealizations = null;
+
+  function parseDate(input: string) {
+    return new Date(input).getTime();
+  }
+  try {
+    // Connect to MongoDB
+    await connectDB();
+    // Access the specified database and collection
+    const db = client.db(dbName);
+    const collection = db.collection(collectionName);
+    // Retrieve all documents in the collection
+    const realizations = await collection.find().toArray();
+    // Sort documents by date
+    sortedrealizations = realizations.sort(
+      (a: { realizationDate: string }, b: { realizationDate: string }) =>
+        parseDate(b.realizationDate) - parseDate(a.realizationDate)
+    );
+  } catch (error) {
+    console.log(error);
+  } finally {
+    // Close the MongoDB connection
+    client.close();
+  }
+  return {
+    props: {
+      realizations: JSON.parse(JSON.stringify(sortedrealizations)),
+    },
+    revalidate: false, //on demand revalidation
+  };
 }
