e9274e90bdea8c863068117b4f26192c0a0f02c2
JKapturkiewicz <kuba.kapek@gmail.com>
2023-07-07 12:38:17 +0200

    Update images remote pattern

--- next.config.js
+++ next.config.js
@@ -6,12 +6,8 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "www.dropbox.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "i.postimg.cc",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "/d/**",
       },
     ],
   },
@@ -30,7 +26,7 @@ const nextConfig = {
           },
           {
             key: "Content-Security-Policy",
-            value: `default-src 'self' https://vitals.vercel-insights.com https://www.google.com/recaptcha/ ; form-action 'self'; frame-src 'self' https://www.google.com/recaptcha/; style-src 'self' https://fonts.googleapis.com 'unsafe-inline'; img-src 'self' https://www.dropbox.com https://stamen-tiles.a.ssl.fastly.net https://a.tile.openstreetmap.org https://b.tile.openstreetmap.org https://c.tile.openstreetmap.org data: ; script-src 'self' ${
+            value: `default-src 'self' https://vitals.vercel-insights.com https://www.google.com/recaptcha/ ; form-action 'self'; frame-src 'self' https://www.google.com/recaptcha/; style-src 'self' https://fonts.googleapis.com 'unsafe-inline'; img-src 'self' https://lh3.googleusercontent.com https://stamen-tiles.a.ssl.fastly.net https://a.tile.openstreetmap.org https://b.tile.openstreetmap.org https://c.tile.openstreetmap.org data: ; script-src 'self' ${
               production ? "" : "'unsafe-eval'"
             } https://apis.google.com  https://vitals.vercel-insights.com/  https://www.google.com/recaptcha/ https://www.gstatic.com/recaptcha/ ;`,
           },
