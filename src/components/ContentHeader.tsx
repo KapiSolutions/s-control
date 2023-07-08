be4c6f1bd9c487f94a75af3f1298598882069bc1
JKapturkiewicz <kuba.kapek@gmail.com>
2023-07-08 20:00:26 +0200

    Change opacity of the second paragraph

--- src/components/ContentHeader.tsx
+++ src/components/ContentHeader.tsx
@@ -12,10 +12,10 @@ const ContentHeader = ({ primary, secondary }: Props): JSX.Element => {
     <Stack direction="row" spacing={2} sx={{mb:4}}>
       <Box sx={{ position: "relative", width: "8px", minHeight: "100%", backgroundColor: "primary.main" }}></Box>
       <Box>
-        <Typography variant="h4" sx={{ textTransform: "uppercase", fontWeight: "bold" }}>
+        <Typography variant="h4" component="h1" sx={{ textTransform: "uppercase", fontWeight: "bold" }}>
           {primary}
         </Typography>
-        <Typography variant="body1">{secondary}</Typography>
+        <Typography variant="body1" sx={{opacity: 0.8}}>{secondary}</Typography>
       </Box>
     </Stack>
   );
