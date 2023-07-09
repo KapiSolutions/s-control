98de71974ac328fa5074ed1ecf78b0f4a63b6b13
JKapturkiewicz <kuba.kapek@gmail.com>
2023-07-09 11:47:11 +0200

    Add url to the google maps navigation

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -22,8 +22,10 @@ const Contact = (): JSX.Element => {
   const isMobile = useMediaQuery(theme.breakpoints.down("md"), {
     defaultMatches: true,
   });
+  const gMapsUrl =
+    "https://www.google.com/maps/dir//%C5%BBarnowiec+119,+38-460+%C5%BBarnowiec/@49.7051489,21.1001723,9z/data=!4m8!4m7!1m0!1m5!1m1!1s0x473c495f118269cb:0xcd02dadc7a7f2822!2m2!1d21.6630658!2d49.693944?entry=ttu";
   return (
-    <Box sx={{maxWidth: "100vw"}}>
+    <Box sx={{ maxWidth: "100vw" }}>
       <Container name="ContactSection">
         <ContentHeader primary="Szybki kontakt" secondary="Skorzystaj z naszego formularza" />
         <Stack alignItems="center" justifyContent="center" direction={isMobile ? "column" : "row"}>
@@ -41,7 +43,12 @@ const Contact = (): JSX.Element => {
                 setZoom(zoom);
               }}
             >
-              <Marker width={50} color="orange" anchor={[49.694, 21.663]} />
+              <Marker
+                width={50}
+                color="orange"
+                anchor={[49.694, 21.663]}
+                onClick={() => window.open(gMapsUrl, "_blank", "noopener,noreferrer")}
+              />
               {zoom === 8 && (
                 <Overlay anchor={[49.98, 21.2]} offset={[120, 79]}>
                   <Box sx={{ position: "relative", width: "200px", height: "200px" }}>
