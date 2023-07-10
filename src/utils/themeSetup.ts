e3348acd860fb52f6f3cbcb40d9628df58fa1415
JKapturkiewicz <kuba.kapek@gmail.com>
2023-07-10 10:10:34 +0200

    Update palette

--- src/utils/themeSetup.ts
+++ src/utils/themeSetup.ts
@@ -2,11 +2,20 @@ const themeSetup = {
   palette: {
     text: {
       primary: "#001525",
+      dark: "#ffffff",
+      darkSecondary: "#a2a7ae",
     },
     primary: {
       main: "#f7b236",
       dark: "#f09e0a",
-      // contrastText: "#f1f1f1",
+    },
+    secondary: {
+      main: "#001525",
+    },
+    layout: {
+      dark: "#172536",
+      semiLight: "#f8f9f9",
+      navbar: "rgba(255, 255, 255, 0.8)"
     },
   },
   typography: {
