10d2f001bbbf7cb24f68821e3251a74f933f1be9
JKapturkiewicz <kuba.kapek@gmail.com>
2023-07-10 21:26:43 +0200

    Use only in the client-side

--- src/utils/hooks/useIsVisible.ts
+++ src/utils/hooks/useIsVisible.ts
@@ -3,9 +3,11 @@ import { useEffect, useState, useMemo } from "react";
 export default function useIsVisible(ref: React.RefObject<Element | null>) {
   const [isIntersecting, setIsIntersecting] = useState(false);
 
-  const observer = useMemo(() => new IntersectionObserver(([entry]) => setIsIntersecting(entry.isIntersecting)), []);
+  const observer = useMemo(() => typeof window !== 'undefined' ?   new IntersectionObserver(([entry]) => setIsIntersecting(entry.isIntersecting)): null, []) ;
 
   useEffect(() => {
+    if (!observer) return;
+    
     const currentRef = ref.current;
     currentRef && observer.observe(ref.current);
 
