cb8d5dfee58526559d41681a100c7cc8ec5a6a47
JKapturkiewicz <kuba.kapek@gmail.com>
2023-07-10 10:09:36 +0200

    Rename file and update Mui Palette interface

---
+++ declarations.d.ts
@@ -0,0 +1,22 @@
+import { AriaAttributes, DOMAttributes } from "react";
+
+declare module 'react' {
+  interface HTMLAttributes<T> extends AriaAttributes, DOMAttributes<T> {
+    name?: string;
+  }
+};
+
+declare module "@mui/material/styles" {
+  interface Palette {
+    text: {
+      dark: string;
+      darkSecondary: string;
+    };
+    layout: {
+      dark: string;
+      semiLight: string;
+      light: string;
+      navbar: string;
+    };
+  }
+}
