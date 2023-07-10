f3ee9347c4c3b33b6c2224255dfe5f5af3492bf5
JKapturkiewicz <kuba.kapek@gmail.com>
2023-07-10 21:18:13 +0200

    Use isVisible hook and add zoom animations

--- src/components/sections/WhyUs.tsx
+++ src/components/sections/WhyUs.tsx
@@ -1,79 +1,83 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Box, Container, Paper, Stack, Typography, useTheme, useMediaQuery } from "@mui/material";
 import ContentHeader from "../ContentHeader";
 import ConstructionIcon from "@mui/icons-material/Construction";
 import AutoAwesomeIcon from "@mui/icons-material/AutoAwesome";
 import AccessibilityNewIcon from "@mui/icons-material/AccessibilityNew";
 import AssuredWorkloadIcon from "@mui/icons-material/AssuredWorkload";
+import useIsVisible from "@/utils/hooks/useIsVisible";
+import Zoom from "@mui/material/Zoom";
 
 const WhyUs = (): JSX.Element => {
   const theme = useTheme();
+  const satisfyBarRef = useRef(null);
+  const ref0 = useRef(null);
+  const ref1 = useRef(null);
+  const ref2 = useRef(null);
+  const ref3 = useRef(null);
   const [progress, setProgress] = useState(10);
-  const [isVisible, setIsVisible] = useState(false);
   const isMobile = useMediaQuery(theme.breakpoints.down("sm"), {
     defaultMatches: true,
   });
+  const iconSize = 80;
 
-  // Check if the progress bar section is visible in the current viewport
-  const checkPosition = () => {
-    const actPos = window.scrollY; //Actual scroll position
-    const windowHeight = window.innerHeight; //Window height
-    const element = document.getElementsByName("satisfyBar")[0];
-    if (element) {
-      const elementPos = element.offsetTop; //Element position
-      // If visible, fire up the progress bar
-      if (actPos + windowHeight >= elementPos) {
-        setIsVisible(true);
-      } else {
-        setIsVisible(false);
-        setProgress(0);
-      }
-    }
-  };
-
-  useEffect(() => {
-    window.addEventListener("scroll", checkPosition);
-    return () => {
-      window.removeEventListener("scroll", checkPosition);
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  const satisfyBarVisible = useIsVisible(satisfyBarRef);
+  const filar0Visible = useIsVisible(ref0);
+  const filar1Visible = useIsVisible(ref1);
+  const filar2Visible = useIsVisible(ref2);
+  const filar3Visible = useIsVisible(ref3);
 
   // Handle Progress Bar
   useEffect(() => {
+    setProgress(0);
     const timer =
-      progress < 100 && isVisible
+      progress < 100 && satisfyBarVisible
         ? setInterval(() => {
             setProgress((prevProgress) => (prevProgress >= 100 ? 100 : prevProgress + 5));
-          }, 50)
+          }, 40)
         : undefined;
     return () => {
       clearInterval(timer);
     };
-  }, [progress, isVisible]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [satisfyBarVisible]);
 
-  const filars = [
-    {
-      icon: <ConstructionIcon sx={{ fontSize: 80 }} />,
-      header: "Jakość",
-      content: "Najwyższe standardy jakości zapewniające trwałość i niezawodność w działaniu",
-    },
-    {
-      icon: <AutoAwesomeIcon sx={{ fontSize: 80, mr: 1 }} />,
-      header: "Estetyka",
-      content: "Dbałość o detale i walory estetyczne, aby nie tylko niższe rachunki cieszyły Twoje oko :)",
-    },
-    {
-      icon: <AccessibilityNewIcon sx={{ fontSize: 80 }} />,
-      header: "Wygoda",
-      content: "Inwestycje dopasujemy do Twoich potrzeb, a formalności załatwimy za Ciebie",
-    },
-    {
-      icon: <AssuredWorkloadIcon sx={{ fontSize: 80 }} />,
-      header: "Bezpieczeństwo",
-      content: "Fachowy montaż to gwarancja bezpiecznej i bezawaryjnej pracy urządzeń",
-    },
-  ];
+  const filar = (
+    ref: React.RefObject<HTMLElement | null>,
+    visible: boolean,
+    icon: JSX.Element,
+    header: string,
+    content: string
+  ) => {
+    return (
+      <Zoom in={visible} ref={ref} timeout={1000}>
+        <Stack
+          alignItems="center"
+          justifyContent="top"
+          sx={{ textAlign: "center", width: isMobile ? "100%" : "23%", minWidth: "250px" }}
+        >
+          <Box
+            sx={{
+              width: "120px",
+              height: "120px",
+              borderRadius: "50%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              backgroundColor: "primary.main",
+              mb: 2,
+            }}
+          >
+            {icon}
+          </Box>
+          <Typography variant="h6" component="p" sx={{ fontWeight: "bold", mb: 1 }}>
+            {header}
+          </Typography>
+          <Typography variant="body1">{content}</Typography>
+        </Stack>
+      </Zoom>
+    );
+  };
   return (
     <Box sx={{ maxWidth: "100vw", mt: 2 }} component="section">
       <Container>
@@ -85,38 +89,38 @@ const WhyUs = (): JSX.Element => {
           justifyContent="center"
           useFlexGap
         >
-          {filars.map((filar, idx) => (
-            <Stack
-              key={idx}
-              alignItems="center"
-              justifyContent="top"
-              sx={{ textAlign: "center", width: isMobile ? "100%" : "23%", minWidth: "250px" }}
-            >
-              <Box
-                sx={{
-                  width: "120px",
-                  height: "120px",
-                  borderRadius: "50%",
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
-                  backgroundColor: "primary.main",
-                  mb: 2,
-                }}
-              >
-                {filar.icon}
-              </Box>
-              <Typography variant="h6" component="p" sx={{ fontWeight: "bold", mb: 1 }}>
-                {filar.header}
-              </Typography>
-              {/* <Typography variant="body1">{filar.content}</Typography> */}
-              <Typography variant="body1">{filar.content}</Typography>
-            </Stack>
-          ))}
+          {filar(
+            ref0,
+            filar0Visible,
+            <ConstructionIcon sx={{ fontSize: iconSize }} />,
+            "Jakość",
+            "Najwyższe standardy jakości zapewniające trwałość i niezawodność w działaniu"
+          )}
+          {filar(
+            ref1,
+            filar1Visible,
+            <AutoAwesomeIcon sx={{ fontSize: iconSize }} />,
+            "Estetyka",
+            "Dbałość o detale i walory estetyczne, aby nie tylko niższe rachunki cieszyły Twoje oko :)"
+          )}
+          {filar(
+            ref2,
+            filar2Visible,
+            <AccessibilityNewIcon sx={{ fontSize: iconSize }} />,
+            "Wygoda",
+            "Inwestycje dopasujemy do Twoich potrzeb, a formalności załatwimy za Ciebie"
+          )}
+          {filar(
+            ref3,
+            filar3Visible,
+            <AssuredWorkloadIcon sx={{ fontSize: iconSize }} />,
+            "Bezpieczeństwo",
+            "Fachowy montaż to gwarancja bezpiecznej i bezawaryjnej pracy urządzeń"
+          )}
         </Stack>
         <Stack alignItems="center" justifyContent="center" mt={6} spacing={1}>
           <Box sx={{ position: "relative", width: "50%", height: "2px", backgroundColor: "primary.main", mb: 3 }}></Box>
-          <Typography name="satisfyBar" variant="h4" component="p">
+          <Typography ref={satisfyBarRef} variant="h4" component="p">
             {progress}%
           </Typography>
           <Typography variant="h5">Zadowolonych klientów</Typography>
