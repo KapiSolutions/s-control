4e8b38dd12747a1f67f731dde26f164f91038b7c
JKapturkiewicz <kuba.kapek@gmail.com>
2023-07-10 15:46:17 +0200

    Add scroll to top action button

--- src/components/CallToUs.tsx
+++ src/components/CallToUs.tsx
@@ -1,16 +1,55 @@
 import React from "react";
 import { Box, Divider, Stack, Typography, useTheme, useMediaQuery } from "@mui/material";
 import CallIcon from "@mui/icons-material/Call";
+import KeyboardDoubleArrowUpIcon from '@mui/icons-material/KeyboardDoubleArrowUp';
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const CallToUs = (): JSX.Element => {
+  const router = useRouter();
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("sm"), {
     defaultMatches: true,
   });
 
+  const style = {
+    diamondShape: {
+      position: "absolute",
+      top: -20,
+      right: isMobile ? "calc(50% - 30px)" : 60,
+      bgcolor: "layout.dark",
+      height: "60px",
+      textAlign: "center",
+      transform: "rotate(45deg)",
+      width: "60px",
+      borderRadius: 4,
+    },
+    diamondContent: {
+      color: "text.dark",
+      display: "table-cell",
+      height: "60px",
+      transform: "rotate(-45deg)",
+      verticalAlign: "middle",
+      width: "60px",
+    },
+  };
+
+  const scrollButtonAction = () => {
+    if (router.pathname === "/") {
+      window.history.pushState(null, "", `/`);
+      window.scrollTo(0, 0);
+    } else {
+      router.push("/");
+    }
+  };
   return (
-    <Box sx={{ width: "100vw", bgcolor: "layout.dark", pt: 8, pb: 8 }}>
+    <Box sx={{ position: "relative", width: "100vw", bgcolor: "layout.dark", pt: 10, pb: 10 }}>
+      <Box sx={style.diamondShape}>
+        <Box sx={style.diamondContent} className="pointer Hover" onClick={scrollButtonAction}>
+        <KeyboardDoubleArrowUpIcon sx={{fontSize: 32 }} />
+        </Box>
+      </Box>
+
       <Stack spacing={4} alignItems="center">
         <Typography variant="h4" component="h1" sx={{ fontWeight: "bold" }}>
           Zadzwoń do nas
@@ -33,7 +72,7 @@ const CallToUs = (): JSX.Element => {
             }}
           >
             <Link href="tel:730530556">
-              <CallIcon color="primary" sx={{ fontSize: 62 }} className="zoom"/>
+              <CallIcon color="primary" sx={{ fontSize: 62 }} className="zoom" />
             </Link>
           </Divider>
         </Box>
