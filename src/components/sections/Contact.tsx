9f08063d711ff8e8e24bd85841d1a906701229e7
JKapturkiewicz <kuba.kapek@gmail.com>
2023-07-11 20:08:51 +0200

    Add slide animation

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -1,11 +1,12 @@
-import React, { useState } from "react";
-import { Box, Stack, Typography, Container, useTheme, useMediaQuery } from "@mui/material";
+import React, { useState, useRef } from "react";
+import { Box, Stack, Container, useTheme, useMediaQuery, Slide } from "@mui/material";
 import dynamic from "next/dynamic";
 import { osm } from "pigeon-maps/providers";
-import { stamenTerrain } from "pigeon-maps/providers";
+// import { stamenTerrain } from "pigeon-maps/providers";
 import ContentHeader from "../ContentHeader";
 import ContactForm from "../ContactForm";
 import { Overlay, Marker } from "pigeon-maps";
+import useIsVisible from "@/utils/hooks/useIsVisible";
 import Image from "next/image";
 import arrow from "../../../public/img/sections/contact/arrow.png";
 
@@ -17,19 +18,28 @@ const MapWrapper = ({ children, ...props }: any) => {
 };
 
 const Contact = (): JSX.Element => {
+  const containerRef = useRef(null);
+  const itemRef = useRef(null);
   const [zoom, setZoom] = useState(8);
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("md"), {
     defaultMatches: true,
   });
+  const itemVisible = useIsVisible(itemRef);
+
   const gMapsUrl =
     "https://www.google.com/maps/dir//%C5%BBarnowiec+119,+38-460+%C5%BBarnowiec/@49.7051489,21.1001723,9z/data=!4m8!4m7!1m0!1m5!1m1!1s0x473c495f118269cb:0xcd02dadc7a7f2822!2m2!1d21.6630658!2d49.693944?entry=ttu";
   return (
     <Box sx={{ maxWidth: "100vw" }} component="section">
-      <Container name="ContactSection">
+      <Container name="ContactSection" ref={containerRef.current}>
         <ContentHeader primary="Szybki kontakt" secondary="Skorzystaj z naszego formularza" />
         <Stack alignItems="center" justifyContent="center" direction={isMobile ? "column" : "row"}>
-          <ContactForm />
+          <Slide direction="right" container={containerRef.current} in={itemVisible} timeout={500}>
+            <Box ref={itemRef}>
+              <ContactForm />
+            </Box>
+          </Slide>
+
           <Box sx={{ width: isMobile ? "100%" : "50%", ml: isMobile ? 0 : 5, mt: isMobile ? 4 : 0 }}>
             <MapWrapper
               provider={osm}
