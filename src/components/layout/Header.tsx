18cff51492e1847c4377e3f0c4e0406fdecd76bd
JKapturkiewicz <kuba.kapek@gmail.com>
2023-07-11 19:40:32 +0200

    Add overlay background gradient

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useRouter } from "next/router";
 import { Box, Button, Typography, Stack, Container, useTheme, useMediaQuery } from "@mui/material";
 import Navbar from "./Navbar";
+import Logotype from "../Logotype";
 
 const Header = (): JSX.Element => {
   const router = useRouter();
@@ -9,10 +10,24 @@ const Header = (): JSX.Element => {
   const isMobile = useMediaQuery(theme.breakpoints.down("sm"), {
     defaultMatches: true,
   });
-
+  const styles = {
+    overlay: {
+      position: "absolute",
+      top: 0,
+      right: 0,
+      bottom: 0,
+      left: 0,
+      background: isMobile ? "none" : "linear-gradient(to right, rgba(255,255,255,0.7) 0%, rgba(0,0,0,0) 50%)",
+      // background: 'linear-gradient(to right, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0) 100%)',
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+    },
+  };
   return (
     <Box
       sx={{
+        position: "relative",
         height: "100%",
         display: "flex",
         justifyContent: "center",
@@ -22,53 +37,53 @@ const Header = (): JSX.Element => {
       }}
     >
       <Navbar />
+      <Box sx={styles.overlay}>
+        <Container>
+          <Stack spacing={isMobile ? 3 : 6} sx={{ textAlign: "left", ml: 2 }}>
+            <Box>
+              <Logotype size={isMobile ? 80 : 100} weight="bold" />
+            </Box>
 
-      <Container>
-        <Stack spacing={isMobile ? 3 : 6} sx={{ textAlign: "left", ml: 2 }}>
-          <Box>
-            <Typography variant="h1" component="h1" sx={{ fontWeight: "bold" }}>
-              S-control
-            </Typography>
-          </Box>
-
-          <Typography
-            color="whitesmoke"
-            variant={isMobile ? "h5" : "h4"}
-            component="p"
-            mb={2}
-            sx={{
-              textTransform: "uppercase",
-              // fontWeight: "bold",
-              backgroundColor: "rgba(0, 0, 0, 0.7)",
-              borderRadius: "4px",
-              p: 2,
-              maxWidth: "90%",
-            }}
-          >
-            Instalacje fotowoltaiczne i pompy ciepła
-          </Typography>
-
-          <Stack direction="row" spacing={2} sx={{ mt: isMobile ? 2 : 0 }}>
-            {/* <Button variant="outlined" size="large" color="secondary">
-              Wycena
-            </Button> */}
-            <Button
-              variant="contained"
-              size="large"
-              color="primary"
-              onClick={() => {
-                if (router.route === "/") {
-                  document.getElementsByName("main")[0].scrollIntoView({ block: "start", inline: "nearest" });
-                } else {
-                  router.push("/#main");
-                }
+            <Typography
+              color="whitesmoke"
+              variant={isMobile ? "h5" : "h4"}
+              component="p"
+              mb={2}
+              sx={{
+                textTransform: "uppercase",
+                background: "linear-gradient(to right, rgba(0,0,0,0.7) 60%, rgba(0,0,0,0) 110%)",
+                // backgroundColor: "rgba(0, 0, 0, 0.7)",
+                borderRadius: "6px",
+                p: 2,
+                maxWidth: "90%",
               }}
             >
-              Dowiedz się więcej
-            </Button>
+              Instalacje fotowoltaiczne i pompy ciepła
+            </Typography>
+
+            <Stack direction="row" spacing={2} sx={{ mt: isMobile ? 2 : 0 }}>
+              {/* <Button variant="outlined" size="large" color="secondary">
+              Wycena
+            </Button> */}
+              <Button
+                variant="contained"
+                size="large"
+                color="primary"
+                sx={{ fontWeight: "bold", width: "220px", height: "50px", borderRadius: "10px" }}
+                onClick={() => {
+                  if (router.route === "/") {
+                    document.getElementsByName("main")[0].scrollIntoView({ block: "start", inline: "nearest" });
+                  } else {
+                    router.push("/#main");
+                  }
+                }}
+              >
+                Dowiedz się więcej
+              </Button>
+            </Stack>
           </Stack>
-        </Stack>
-      </Container>
+        </Container>
+      </Box>
     </Box>
   );
 };
