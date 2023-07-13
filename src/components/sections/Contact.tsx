04ed2c616c11208a52c3b3f2b5a27bf99761a4a2
JKapturkiewicz <kuba.kapek@gmail.com>
2023-07-13 15:43:06 +0200

    Update animations

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -30,45 +30,47 @@ const Contact = (): JSX.Element => {
   const gMapsUrl =
     "https://www.google.com/maps/dir//%C5%BBarnowiec+119,+38-460+%C5%BBarnowiec/@49.7051489,21.1001723,9z/data=!4m8!4m7!1m0!1m5!1m1!1s0x473c495f118269cb:0xcd02dadc7a7f2822!2m2!1d21.6630658!2d49.693944?entry=ttu";
   return (
-    <Box sx={{ maxWidth: "100vw" }} component="section">
-      <Container name="ContactSection" ref={containerRef.current}>
+    <Box sx={{ maxWidth: "100vw" }} component="section" ref={containerRef.current}>
+      <Container name="ContactSection">
         <ContentHeader primary="Szybki kontakt" secondary="Skorzystaj z naszego formularza" />
-        <Stack alignItems="center" justifyContent="center" direction={isMobile ? "column" : "row"}>
+        <Box ref={itemRef}>
           <Slide direction="right" container={containerRef.current} in={itemVisible} timeout={500}>
-            <Box ref={itemRef}>
-              <ContactForm />
-            </Box>
-          </Slide>
+            <Stack alignItems="center" justifyContent="center" direction={isMobile ? "column" : "row"}>
+              <Box>
+                <ContactForm />
+              </Box>
 
-          <Box sx={{ width: isMobile ? "100%" : "50%", ml: isMobile ? 0 : 5, mt: isMobile ? 4 : 0 }}>
-            <MapWrapper
-              provider={osm}
-              height={280}
-              defaultCenter={[49.85, 21.4]}
-              metaWheelZoom={true}
-              zoom={zoom}
-              maxZoom={14}
-              animate={false}
-              onBoundsChanged={({ zoom }: { zoom: number }) => {
-                setZoom(zoom);
-              }}
-            >
-              <Marker
-                width={50}
-                color="orange"
-                anchor={[49.694, 21.663]}
-                onClick={() => window.open(gMapsUrl, "_blank", "noopener,noreferrer")}
-              />
-              {zoom === 8 && (
-                <Overlay anchor={[49.98, 21.2]} offset={[120, 79]}>
-                  <Box sx={{ position: "relative", width: "200px", height: "200px" }}>
-                    <Image src={arrow} fill alt="Tu jesteśmy" style={{ objectFit: "contain" }} />
-                  </Box>
-                </Overlay>
-              )}
-            </MapWrapper>
-          </Box>
-        </Stack>
+              <Box sx={{ width: isMobile ? "100%" : "50%", ml: isMobile ? 0 : 5, mt: isMobile ? 4 : 0 }}>
+                <MapWrapper
+                  provider={osm}
+                  height={280}
+                  defaultCenter={[49.85, 21.4]}
+                  metaWheelZoom={true}
+                  zoom={zoom}
+                  maxZoom={14}
+                  animate={false}
+                  onBoundsChanged={({ zoom }: { zoom: number }) => {
+                    setZoom(zoom);
+                  }}
+                >
+                  <Marker
+                    width={50}
+                    color="orange"
+                    anchor={[49.694, 21.663]}
+                    onClick={() => window.open(gMapsUrl, "_blank", "noopener,noreferrer")}
+                  />
+                  {zoom === 8 && (
+                    <Overlay anchor={[49.98, 21.2]} offset={[120, 79]}>
+                      <Box sx={{ position: "relative", width: "200px", height: "200px" }}>
+                        <Image src={arrow} fill alt="Tu jesteśmy" style={{ objectFit: "contain" }} />
+                      </Box>
+                    </Overlay>
+                  )}
+                </MapWrapper>
+              </Box>
+            </Stack>
+          </Slide>
+        </Box>
       </Container>
     </Box>
   );
