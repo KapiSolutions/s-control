46266410e0199bc9b9c65cf33747fa38f4d4cf51
JKapturkiewicz <kuba.kapek@gmail.com>
2023-07-14 10:21:24 +0200

    Update content and use Grid insted of Stack

--- src/components/sections/Photovoltaics.tsx
+++ src/components/sections/Photovoltaics.tsx
@@ -1,10 +1,14 @@
 import React, { useRef } from "react";
-import { Box, Container, Stack, Typography, useTheme, useMediaQuery, Zoom } from "@mui/material";
+import { Box, Container, Stack, Typography, useTheme, useMediaQuery, Zoom, Grid } from "@mui/material";
 import ContentHeader from "../ContentHeader";
 import useIsVisible from "@/utils/hooks/useIsVisible";
 import Image from "next/image";
 import img1 from "../../../public/img/sections/photovoltaics/img2.webp";
 import img2 from "../../../public/img/sections/photovoltaics/img3.webp";
+import SolarPowerIcon from "@mui/icons-material/SolarPower";
+import EnergySavingsLeafIcon from "@mui/icons-material/EnergySavingsLeaf";
+import EmojiNatureIcon from "@mui/icons-material/EmojiNature";
+import VerifiedIcon from "@mui/icons-material/Verified";
 
 const Photovoltaics = (): JSX.Element => {
   const itemRef = useRef(null);
@@ -14,25 +18,26 @@ const Photovoltaics = (): JSX.Element => {
   });
   const itemVisible = useIsVisible(itemRef);
   return (
-    <Box component="section" sx={{pt: 6, pb: 6}}>
+    <Box component="section" sx={{ pt: 6, pb: 6 }}>
       <Container sx={{ minHeight: "100vh" }} name="PhotovoltaicsSection">
         <ContentHeader primary="Instalacje Fotowoltaiczne" secondary="Zielona energia dla Twojego domu" />
-        <Stack
+        <Grid
+          container
           direction="row"
           spacing={isMobile ? 2 : 4}
           alignItems="center"
           justifyContent="center"
-          useFlexGap
-          flexWrap="wrap"
           ref={itemRef}
         >
           <Zoom in={itemVisible} timeout={500}>
-            <Box
+            <Grid
+              item
+              xs={12}
+              md={6}
               sx={{
                 position: "relative",
                 height: isMobile ? "330px" : "500px",
-                width: isMobile ? "98%" : "45%",
-                minWidth: isMobile ? "auto" : "350px",
+                maxWidth: isMobile ? "94vw" : "auto",
                 overflow: "hidden",
               }}
             >
@@ -58,19 +63,52 @@ const Photovoltaics = (): JSX.Element => {
               >
                 <Image src={img2} fill alt="s-control fotowoltaika" style={{ objectFit: "cover" }} />
               </Box>
-            </Box>
+            </Grid>
           </Zoom>
-          <Box sx={{ width: isMobile ? "100%" : "50%" }}>
-            <Typography variant="body1">
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-              industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
-              scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release
-              of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software
-              like Aldus PageMaker including versions of Lorem Ipsum.
-            </Typography>
-          </Box>
-        </Stack>
+          <Grid item xs={12} md={6}>
+            <Stack
+              spacing={2}
+              alignItems={isMobile ? "center" : "left"}
+              sx={{ textAlign: isMobile ? "center" : "left", pr: 1 }}
+            >
+              <Stack direction={isMobile ? "column" : "row"} spacing={2} alignItems="center">
+                <SolarPowerIcon color="primary" sx={{ fontSize: 36 }} />
+                <Typography variant="body1" sx={{ fontWeight: "bold", textTransform: "uppercase" }}>
+                  Nasza Wizja
+                </Typography>
+              </Stack>
+              <Typography variant="body1" color="text.secondary">
+                W S-Control zdajemy sobie sprawę, że energia słoneczna jest nie tylko przyszłością, ale także
+                rozwiązaniem na teraźniejszość. Dlatego specjalizujemy się w dostarczaniu kompleksowych rozwiązań
+                fotowoltaicznych, które pozwolą Ci cieszyć się czystą energią i oszczędnościami na rachunkach za prąd.
+              </Typography>
+
+              <Stack direction={isMobile ? "column" : "row"} spacing={2} alignItems="center">
+                <EmojiNatureIcon color="primary" sx={{ fontSize: 36 }} />
+                <Typography variant="body1" sx={{ fontWeight: "bold", textTransform: "uppercase" }}>
+                  Środowisko
+                </Typography>
+              </Stack>
+              <Typography variant="body1" color="text.secondary">
+                Dbamy o czystość naszego środowiska i przyczyniamy się do walki ze zmianami klimatu. Wykorzystując
+                energię słoneczną, nie emitujemy żadnych szkodliwych gazów cieplarnianych ani innych substancji
+                szkodliwych dla atmosfery.
+              </Typography>
+              
+              <Stack direction={isMobile ? "column" : "row"} spacing={2} alignItems="center">
+                <VerifiedIcon color="primary" sx={{ fontSize: 36 }} />
+                <Typography variant="body1" sx={{ fontWeight: "bold", textTransform: "uppercase" }}>
+                  Odpowiedzialność
+                </Typography>
+              </Stack>
+              <Typography variant="body1" color="text.secondary">
+                Jesteśmy dumni z jakości naszych produktów i usług. Stawiamy na sprawdzone technologie i wysokiej
+                jakości panele fotowoltaiczne, które są gwarancją niezawodności i wydajności przez wiele lat. Nasze
+                instalacje są również objęte gwarancją, aby zapewnić Ci spokój i pewność, że podejmujesz trafną decyzję.
+              </Typography>
+            </Stack>
+          </Grid>
+        </Grid>
       </Container>
     </Box>
   );
