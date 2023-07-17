524eaa8a41a3f06032bd7f470e4b71f759dbcc83
JKapturkiewicz <kuba.kapek@gmail.com>
2023-07-17 12:31:47 +0200

    Change msg and email fields to be optional

--- src/utils/schema/contact.ts
+++ src/utils/schema/contact.ts
@@ -12,9 +12,9 @@ export type Contact = {
 const reqMsg = "Pole wymagane.";
 export const schema = yup.object().shape({
   clientName: yup.string().required(reqMsg).max(50, "Zbyt wiele znaków."),
-  email: yup.string().required(reqMsg).min(3, "Niepoprawny adres e-mail.").max(50, "Zbyt wiele znaków.").email("Niepoprawny adres e-mail"),
+  email: yup.string().max(50, "Zbyt wiele znaków.").email("Niepoprawny adres e-mail"),
   telephone: yup.string().required(reqMsg).min(9, "Niepoprawny numer telefonu").max(15, "Zbyt wiele znaków."),
 //   city: yup.string().required(reqMsg).max(20, "Zbyt wiele znaków."),
   postalCode: yup.string().required(reqMsg).max(7, "Zbyt wiele znaków."),
-  msg: yup.string().required(reqMsg).max(500, "Zbyt wiele znaków."),
+  msg: yup.string().max(500, "Zbyt wiele znaków."),
 });
