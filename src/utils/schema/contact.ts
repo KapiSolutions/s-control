8b3b4e98a3fa68ba6abb1831530f004da6c6401d
JKapturkiewicz <kuba.kapek@gmail.com>
2023-07-17 12:04:48 +0200

    Add telephone and postal code

--- src/utils/schema/contact.ts
+++ src/utils/schema/contact.ts
@@ -3,9 +3,9 @@ import * as yup from "yup";
 export type Contact = {
   clientName: string;
   email: string;
-//   telephone: string;
+  telephone: string;
 //   city: string;
-//   postalCode: string;
+  postalCode: string;
   msg: string;
 };
 
@@ -13,8 +13,8 @@ const reqMsg = "Pole wymagane.";
 export const schema = yup.object().shape({
   clientName: yup.string().required(reqMsg).max(50, "Zbyt wiele znaków."),
   email: yup.string().required(reqMsg).min(3, "Niepoprawny adres e-mail.").max(50, "Zbyt wiele znaków.").email("Niepoprawny adres e-mail"),
-//   telephone: yup.string().required(reqMsg).min(9, "Niepoprawny numer telefonu").max(15, "Zbyt wiele znaków."),
+  telephone: yup.string().required(reqMsg).min(9, "Niepoprawny numer telefonu").max(15, "Zbyt wiele znaków."),
 //   city: yup.string().required(reqMsg).max(20, "Zbyt wiele znaków."),
-//   postalCode: yup.string().required(reqMsg).max(7, "Zbyt wiele znaków."),
+  postalCode: yup.string().required(reqMsg).max(7, "Zbyt wiele znaków."),
   msg: yup.string().required(reqMsg).max(500, "Zbyt wiele znaków."),
 });
