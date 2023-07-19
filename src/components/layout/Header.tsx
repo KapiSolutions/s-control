b51cc306a1995a8d32637d574bbe0dfed69f53bd
JKapturkiewicz <kuba.kapek@gmail.com>
2023-07-19 13:24:56 +0200

    Add animation to the typography

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useRouter } from "next/router";
 import { Box, Button, Typography, Stack, Container, useTheme, useMediaQuery } from "@mui/material";
+import { TypeAnimation } from "react-type-animation";
 import Navbar from "./Navbar";
 import Logotype from "../Logotype";
 
@@ -24,30 +25,49 @@ const Header = (): JSX.Element => {
       justifyContent: "center",
       alignItems: "center",
     },
+    wrapper: {
+      position: "relative",
+      height: "100%",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundImage: "url('/img/main-2.webp')",
+      backgroundPosition: "right",
+      backgroundSize: "cover",
+    },
   };
   const scrollToSection = (name: string) => {
     if (router.pathname === "/") {
       const element = document.getElementsByName(name)[0];
       window.history.pushState(null, "", `/#${name}`); //add to history without loading the page
-      // window.history.replaceState({ ...window.history.state, as: newUrl, url: newUrl }, '', newUrl);
       window.scrollTo({ top: element.offsetTop - scrollOffset, behavior: "smooth" });
     } else {
       router.push(`/#${name}`, undefined, { scroll: false });
     }
   };
+  const dynamicText = (): JSX.Element => {
+    return (
+      <TypeAnimation
+        preRenderFirstString={true}
+        sequence={[
+          1500,
+          "pompy ciepła",
+          1500, // Waits 1s
+          "magazyny energii",
+          1500, // Waits 2s
+          "systemy hems",
+          1500, // Waits 2s
+          "klimatyzacje",
+        ]}
+        wrapper="span"
+        cursor={true}
+        repeat={Infinity}
+        style={{ display: "inline-block" }}
+      />
+    );
+  };
   return (
-    <Box
-      sx={{
-        position: "relative",
-        height: "100%",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundImage: "url('/img/main-2.webp')",
-        backgroundPosition: "right",
-        backgroundSize: "cover",
-      }}
-    >
+    <Box sx={styles.wrapper}>
       <Navbar />
       <Box sx={styles.overlay}>
         <Container>
@@ -55,29 +75,32 @@ const Header = (): JSX.Element => {
             <Box sx={{ color: "common.white" }}>
               <Logotype size={isMobile ? 80 : 100} weight="bold" />
             </Box>
-
-            <Typography
-              color="whitesmoke"
-              variant={isMobile ? "h5" : "h4"}
-              component="p"
+            <Stack
+              direction={isMobile ? "column" : "row"}
+              alignItems="left"
+              spacing={isMobile ? 0 : 1}
               mb={2}
+              p={2}
               sx={{
-                textTransform: "uppercase",
                 background: "linear-gradient(to right, rgba(0,0,0,0.7) 60%, rgba(0,0,0,0) 110%)",
-                // backgroundColor: "rgba(0, 0, 0, 0.7)",
                 borderRadius: "6px",
-                p: 2,
                 maxWidth: "90%",
+                textTransform: "uppercase",
               }}
             >
-              Instalacje fotowoltaiczne i pompy ciepła
-            </Typography>
+              <Typography color="whitesmoke" variant={isMobile ? "h5" : "h4"} component="p">
+                Instalacje fotowoltaiczne i
+              </Typography>
+              <Typography color="whitesmoke" variant={isMobile ? "h5" : "h4"} component="p">
+                {dynamicText()}
+              </Typography>
+            </Stack>
 
             <Stack
               direction={isMobile ? "column" : "row-reverse"}
-              spacing={2}
+              spacing={4}
               justifyContent="left"
-              sx={{ mt: isMobile ? 2 : 0 }}
+              mt={isMobile ? 2 : 0}
             >
               <Button
                 variant="contained"
