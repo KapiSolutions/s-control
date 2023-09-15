7bfd4538e2c3f84b4429216cb1b6faef83ce257b
JKapturkiewicz <kuba.kapek@gmail.com>
2023-09-15 17:35:26 +0200

    Update Contact type definition

--- src/utils/schema/contact.ts
+++ src/utils/schema/contact.ts
@@ -1,20 +1,19 @@
 import * as yup from "yup";
 
 export type Contact = {
-  clientName: string;
-  email: string;
+  clientName: string ;
+  email: string | undefined;
   telephone: string;
-//   city: string;
   postalCode: string;
-  msg: string;
+  msg: string | undefined;
 };
 
 const reqMsg = "Pole wymagane.";
+
 export const schema = yup.object().shape({
   clientName: yup.string().required(reqMsg).max(50, "Zbyt wiele znaków."),
   email: yup.string().max(50, "Zbyt wiele znaków.").email("Niepoprawny adres e-mail"),
   telephone: yup.string().required(reqMsg).min(9, "Niepoprawny numer telefonu").max(15, "Zbyt wiele znaków."),
-//   city: yup.string().required(reqMsg).max(20, "Zbyt wiele znaków."),
   postalCode: yup.string().required(reqMsg).max(7, "Zbyt wiele znaków."),
   msg: yup.string().max(500, "Zbyt wiele znaków."),
 });
